7910b9d7729b0e2a1d8302d693fd4c2a08ca29c7
Alvaro Uría <alvaro.uria@canonical.com>
2016-03-08 13:58:25 +0100

    requires keystone-admin:identity-admin

---
+++ requires.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from charms.reactive import RelationBase
+from charms.reactive import hook
+from charms.reactive import scopes
+
+
+class KeystoneRequires(RelationBase):
+    scope = scopes.GLOBAL
+
+    # These remote data fields will be automatically mapped to accessors
+    # with a basic documentation string provided.
+
+    auto_accessors = ['private-address', 'service_host', 'service_protocol',
+                      'service_port', 'service_tenant', 'service_username',
+                      'service_password', 'service_tenant_id', 'auth_host',
+                      'auth_protocol', 'auth_port', 'admin_token', 'ssl_key',
+                      'ca_cert', 'ssl_cert', 'https_keystone',
+                      'ssl_cert_admin', 'ssl_cert_internal',
+                      'ssl_cert_public', 'ssl_key_admin', 'ssl_key_internal',
+                      'ssl_key_public']
+
+    @hook('{requires:keystone}-relation-{joined,changed}')
+    def changed(self):
+        conv = self.conversation()
+        self.kst_data = self.auth_data_complete(conv)
+        if self.kst_data:
+            conv.set_state('{relation_name}.available')
+        else:
+            conv.remove_state('{relation_name}.available')
+
+    @hook('{requires:keystone}-relation-{broken,departed}')
+    def departed(self):
+        conv = self.conversation()
+        conv.remove_state('{relation_name}.available')
+
+    def auth_data_complete(self, conv):
+        data = {
+            'service_hostname': conv.get_remote('service_hostname'),
+            'service_port': conv.get_remote('service_port'),
+            'service_username': conv.get_remote('service_username'),
+            'service_password': conv.get_remote('service_password'),
+            'service_tenant_name': conv.get_remote('service_tenant_name')
+        }
+        if all(data.values()):
+            return data
+        return False
+
+    def credentials(self):
+        """
+        Returns a dict of keystone admin credentials
+        """
+        return self.kst_data
